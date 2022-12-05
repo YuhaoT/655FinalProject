21f7271cc4f025e609055cc8e6305ed084eeabd3
lumianious <lumianlq@gmail.com>
2022-12-05 17:05:57 -0500

    added the machine learning part and the server connection part

---
+++ ml/googlenet.py
@@ -0,0 +1,33 @@
+import torch
+from PIL import Image
+from torchvision.models import googlenet
+from torchvision import transforms
+
+def image_predict(image):
+    model = googlenet(weights="GoogLeNet_Weights.IMAGENET1K_V1")
+    model.eval()
+
+    filename = image
+
+    input_image = Image.open(filename)
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(input_batch)
+
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+
+    with open("imagenet_classes.txt", "r") as f:
+        categories = [s.strip() for s in f.readlines()]
+
+    prob, catid = torch.topk(probabilities, 1)
+    res = categories[catid] + " " + str(prob.item())
+    return res
+
---
+++ ml/server.py
@@ -0,0 +1,42 @@
+import socket
+import sys
+import time
+import googlenet
+
+
+def start_connection():
+    address = "127.0.0.1"
+    port = 5005
+    try:
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.bind((address, port))
+        s.listen()
+    except:
+        print('Failed to start connection')
+        sys.exit(1)
+    
+    print ("opened on address {}, port {}, waiting for a connection".format(self.address, self.port))
+
+    while True:
+        sock, addr = s.accept()
+        image_process(sock, addr)
+
+def image_process(sock, addr):
+    print ("connection accpeted {}:{}".format(addr[0],addr[1]))
+    while True:
+        data = sock.recv(40960000)
+        fp = open("image.jpg","wb")
+        fp.write(data)
+        fp.close()
+        start = time.perf_counter()
+        replyStr = googlenet.image_predict("image.jpg")
+        end = time.perf_counter()
+        sock.sendall(replyStr + f" {start - end:0.4f} seconds")
+        sock.close()
+        break
+
+if __name__ == '__main__':
+    start_connection()
+        
+
+
