efd27ac60bcfc1e975107cc60a3d84e6484ddf46
YuhaoT <heyuhao97@gamil.com>
2022-12-06 23:39:17 -0500

    fix image uploader

--- WebServer/file_upload.py
+++ WebServer/file_upload.py
@@ -2,22 +2,21 @@ import os
 from flask import Flask, flash, request, redirect, url_for , render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = './static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-app = Flask(__name__, template_folder='templates', )
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'my_secret_key'
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
-def index():
-    return render_template('./index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    print(request.files)
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -34,7 +33,12 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return render_template('index.html', success=True, filename=filename)
 
+    return render_template('./index.html')
 
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == "__main__":
     app.run(debug=True)
