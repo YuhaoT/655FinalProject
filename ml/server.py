9c3b79261e6f4113ad9b878def8f854ef1405d23
lumianious <lumianlq@gmail.com>
2022-12-07 15:17:22 -0500

    30 second timeouts

--- ml/server.py
+++ ml/server.py
@@ -10,6 +10,7 @@ def start_connection():
     try:
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.bind((address, port))
+        s.settimeout(30)
         s.listen()
     except:
         print('Failed to start connection')
@@ -18,8 +19,12 @@ def start_connection():
     print ("opened on address {}, port {}, waiting for a connection".format(address, port))
 
     while True:
-        sock, addr = s.accept()
-        image_process(sock, addr)
+        try:
+            sock, addr = s.accept()
+            image_process(sock, addr)
+        except socket.timeout:
+            s.close()
+            break
 
 def image_process(sock, addr):
     print ("connection accpeted {}:{}".format(addr[0],addr[1]))
