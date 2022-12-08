f12856cbc3f433e5924644ea1e98d1d438c74265
hey1 <hey1@pcvm1-19.instageni.rnoc.gatech.edu>
2022-12-07 23:17:23 -0500

    update app run

--- WebServer/file_upload.py
+++ WebServer/file_upload.py
@@ -48,4 +48,4 @@ def display_image(filename):
 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     app.run(host = '0.0.0.0',port = 80,debug=True)
